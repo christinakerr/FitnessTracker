adc429a66de1cf59ac235d8a7252ca352db96602
Christi Kerr <christinakerr394@gmail.com>
2020-11-10 16:10:04 -0600

    Established node connection. Fixes #1

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// Add link to models
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+// mongoose connection
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+});
