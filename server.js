d2f1791e7aa4d423f7febb9a313e54f5ef2e772c
Christi Kerr <christinakerr394@gmail.com>
2020-11-11 16:38:33 -0600

    Added html routes

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+require("./routes/html-routes.js")(app);
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout")
 
 app.listen(PORT, () => {
